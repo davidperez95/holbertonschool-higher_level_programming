855a0f88fede511cbfee36827e86036c64f3a5af
David Perez <daperezcruz@gmail.com>
2022-09-22 23:46:42 -0500

    fix on task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,12 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for item in row:
-            if item == row[-1]:
-                print("{:d}".format(item))
-            else:
-                print("{:d}".format(item), end=' ')
+    if matrix:
+        for row in matrix:
+            for item in row:
+                if item == row[-1]:
+                    print("{:d}".format(item))
+                else:
+                    print("{:d}".format(item), end=' ')
+    else:
+        print()
