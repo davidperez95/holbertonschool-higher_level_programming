eff1b431a68bf7b2a67d680d472b3f55f9d269f8
David Perez <daperezcruz@gmail.com>
2022-09-23 15:14:19 -0500

    task 10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+from types import new_class
+
+
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for i in my_list:
+        if (my_list[i] % 2 == 0):
+            new_list.append(True)
+        else:
+            new_list.append(False)
+
+    return new_list
