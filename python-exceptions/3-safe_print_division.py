e9baba7cc9a5e8dba6cc47b671d520fc230aa48f
David Perez <daperezcruz@gmail.com>
2022-09-27 21:48:41 -0500

    task 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {:d}".format(result))
+        return result
