a11541cb21c2364d9c4fc813951c9bc30289a041
David Perez <daperezcruz@gmail.com>
2022-09-27 20:47:55 -0500

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 index += 1
         except IndexError:
-            print("list index out of range")
+            pass
 
     print()
     return index
