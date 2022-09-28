2b8b95fafa0231a7f7684036a6c7fa3a52c07740
David Perez <daperezcruz@gmail.com>
2022-09-27 20:52:39 -0500

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 index += 1
         except IndexError:
-            continue
+            print("Traceback (most recent call last):")
 
     print()
     return index
