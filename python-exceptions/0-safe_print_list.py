f35251c64c3a175747d390786bc490eb91f6f2de
David Perez <daperezcruz@gmail.com>
2022-09-27 20:19:47 -0500

    task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    
+    index = 0
+    try:
+        for i in my_list:
+            if index < x:
+                print("{}".format(my_list[index]))
+                index += 1
+
+    except IndexError:
+        print("list out of index")
+
+    return index
