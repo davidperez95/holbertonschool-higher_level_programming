2877896aa6ea8db04cd4f2d7b7f5610705e43312
David Perez <daperezcruz@gmail.com>
2022-09-28 21:47:01 -0500

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines de Square class"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initializes the square with private size"""
+        if (type(size) is int):
+            self._Square__size = size
+        else:
+            raise TypeError("size must be an integer")
+        
+        if size < 0:
+            raise ValueError("size must be >= 0")
