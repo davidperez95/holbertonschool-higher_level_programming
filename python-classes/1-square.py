5c9717bff13332890b70bf1c46172f4ab95144b2
David Perez <daperezcruz@gmail.com>
2022-09-28 21:34:28 -0500

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines de Square class"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """Initializes the square with private size"""
+        self._Square_size = size
