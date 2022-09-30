73b9b738025b289018cde795ffa0c409d42e0727
David Perez <daperezcruz@gmail.com>
2022-09-29 22:33:32 -0500

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -54,6 +54,6 @@ class Square:
 
         for i in range(self.__position[1]):
             print()
-        for i in range(self.__size):
-            print("".join([" " for j in range(self.__position[0])]), end="")
-            print("".join(["#" for k in range(self.__size)]))
+        for j in range(self.__size):
+            print("".join([" " for k in range(self.__position[0])]), end="")
+            print("".join(["#" for l in range(self.__size)]))
