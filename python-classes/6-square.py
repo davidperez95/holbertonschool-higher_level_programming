3d76c03a80d87a479f777ab879e3bde85e4b3112
David Perez <daperezcruz@gmail.com>
2022-09-29 20:51:32 -0500

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -12,21 +12,29 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = size
+        self.size = size
 
-        if (type(position) is tuple and position[0] is int and position[1] is int):
-            self.__position = position
-        else:
+        if (type(position) is not tuple and position[0] is not int
+            and position[1] is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
-
+            
+        self.position = position
+            
+    """Getter methods"""
     @property
     def size(self):
         """Getter method to return the size"""
         return self.__size
+    
+    @property
+    def position(self):
+        """Getter method to return the position"""
+        return self.__position
 
+    """Setter methods"""
     @size.setter
     def size(self, value):
-        """Setter method to modify the size"""
+        """Setter method to modify the size value"""
         if (type(value) is not int):
             raise TypeError("size must be an integer")
 
@@ -35,6 +43,16 @@ class Square:
 
         self.__size = value
 
+    @position.setter
+    def position(self, value):
+        """Setter method to modify the position value"""
+        if (type(value) is not tuple and value[0] is not int
+            and value[1] is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+            
+        self.__position = value
+
+    """Public instance methods"""
     def area(self):
         """Returns the area of the square"""
         return (self.__size * self.__size)
@@ -45,6 +63,9 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                for j in range(self.__size):
+                for i in range(self.__size):
+                    if (self.__position[0] > 0):
+                        for i in range(self.__position[0]):
+                            print(" ", end='')
                     print("#", end='')
                 print()
