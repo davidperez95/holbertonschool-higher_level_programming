ec2f9ad9a9a20ee13b9d74bea9bf573041486960
David Perez <daperezcruz@gmail.com>
2022-10-04 23:06:42 -0500

    task 1 tests and style fix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,25 +5,26 @@
 def matrix_divided(matrix, div):
     """Function that checks matrix and pass div to the sublist"""
 
-    type_error_str = "matrix must be a matrix (list of lists) of integers/floats"
-    
+    type_error_str = "matrix must be a matrix (list of lists)\
+        of integers/floats"
+
     """Verify div type"""
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
     """Verify div value"""
-    if div <= 0:
+    if div == 0:
         raise ZeroDivisionError("division by zero")
 
     """Verify if matrix is a list"""
     if type(matrix) is not list:
         raise TypeError(type_error_str)
-    
+
     """Verify if matrix contain only lists"""
     for i in matrix:
         if type(i) is not list:
             raise TypeError(type_error_str)
-    
+
     """Verify if the lists in matrix have the same length"""
     lists_length = len(matrix[0])
     for i in matrix:
@@ -34,7 +35,7 @@ def matrix_divided(matrix, div):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if type(matrix[i][j]) is not int and \
-                type(matrix[i][j]) is not float:
+                    type(matrix[i][j]) is not float:
                 raise TypeError(type_error_str)
 
     """Execute the division"""
@@ -45,4 +46,3 @@ def matrix_divided(matrix, div):
             new_matrix[i][j] = round(matrix[i][j] / div, 2)
 
     return new_matrix
-        
