a79819fd441d568b71874afe7dfe5c21b2c13933
David Perez <daperezcruz@gmail.com>
2022-10-05 12:40:09 -0500

    task 3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module prints a square"""
+
+
+def print_square(size):
+    """This function prints a square based no size"""
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    elif size == 0:
+        print("")
+        return
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print("")
