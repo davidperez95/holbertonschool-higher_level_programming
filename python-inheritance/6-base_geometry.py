85bc0353ab3580a93a5d23a1cf11a4f913c5b549
David Perez <daperezcruz@gmail.com>
2022-10-14 17:08:53 -0500

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines the BaseGeometry Class"""
+
+
+class BaseGeometry():
+    """super class BaseGeometry"""
+    def __init__(self):
+        """pass on create a new instance"""
+        pass
+
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
