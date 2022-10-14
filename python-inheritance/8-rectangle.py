da60be31449885e1d063070687a64a287985e669
David Perez <daperezcruz@gmail.com>
2022-10-14 17:38:59 -0500

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module that defines the BaseGeometry Class"""
+
+
+from turtle import width
+
+
+class BaseGeometry():
+    """super class BaseGeometry"""
+    def __init__(self):
+        """pass on create a new instance"""
+        pass
+
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """method that validates value"""
+        if (type(value) is not int):
+            raise TypeError("{} must be an integer".format(name))
+        if (value <= 0):
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
