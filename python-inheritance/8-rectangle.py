ddb0ba43b93154494f3ba07ad8efd6c22eba773d
David Perez <daperezcruz@gmail.com>
2022-10-14 17:43:37 -0500

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines the BaseGeometry Class"""
+"""Module that defines the Rectangle subclass"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
-
-class BaseGeometry():
-    """super class BaseGeometry"""
-    def __init__(self):
-        """pass on create a new instance"""
-        pass
-
-    def area(self):
-        """raise an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method that validates value"""
-        if (type(value) is not int):
-            raise TypeError("{} must be an integer".format(name))
-        if (value <= 0):
-            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
