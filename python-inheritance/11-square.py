94575e42b771d15db04b1184ba447246d828aa78
David Perez <daperezcruz@gmail.com>
2022-10-14 17:59:26 -0500

    task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -16,4 +16,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return the Square description"""
-        return "[Square] {}/{}".format(self.__width, self.__height)
+        return "[Square] {}/{}".format(self.__size, self.__size)
