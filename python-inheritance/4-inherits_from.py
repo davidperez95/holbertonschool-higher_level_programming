21bb659076a6c7c010e16f7c077c83d45ffd08dd
David Perez <daperezcruz@gmail.com>
2022-10-14 16:59:06 -0500

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines the inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """return True if obj is a subclass of a_class, otherwise false"""
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
