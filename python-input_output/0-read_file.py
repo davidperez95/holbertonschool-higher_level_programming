1c327e6e87eaa46bdf9b7733cf405c68046e07d5
David Perez <daperezcruz@gmail.com>
2022-10-14 20:47:27 -0500

    python input and output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as my_file:
-        print(my_file.read())
+        print(my_file.read(), end="")
