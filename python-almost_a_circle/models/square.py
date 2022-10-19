15c2efae3b0f9bdab65b4561a3dd4c6f708bf07d
David Perez <daperezcruz@gmail.com>
2022-10-19 17:57:47 -0500

    task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -25,3 +25,25 @@ class Square(Rectangle):
                                                          self.x,
                                                          self.y,
                                                          self.size)
+
+    def update(self, *args, **kwargs):
+        """Update instance attributes"""
+        if len(args) > 0:
+            for index, value in enumerate(args):
+                if index == 0:
+                    self.id = value
+                elif index == 1:
+                    self.size = value
+                elif index == 2:
+                    self.x = value
+                elif index == 3:
+                    self.y = value
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
