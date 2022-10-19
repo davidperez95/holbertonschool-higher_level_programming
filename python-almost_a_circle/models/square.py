fe95918119d43ee9fedf0424b7c060ee7e0b3f1c
David Perez <daperezcruz@gmail.com>
2022-10-19 17:54:57 -0500

    task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,6 +10,15 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return a Square definition"""
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
