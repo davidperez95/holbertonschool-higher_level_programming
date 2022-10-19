a0c5965845e5ba7ed426228206b5f529becf86ff
David Perez <daperezcruz@gmail.com>
2022-10-19 18:05:31 -0500

    task 14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -47,3 +47,11 @@ class Square(Rectangle):
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        new_dict = {}
+        new_dict["id"] = self.id
+        new_dict["size"] = self.size
+        new_dict["x"] = self.x
+        new_dict["y"] = self.y
+        return new_dict
