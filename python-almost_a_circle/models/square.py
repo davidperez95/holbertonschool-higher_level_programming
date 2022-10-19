1b141a586cb509fbfc2d96ba4e30e274f6162f5d
David Perez <daperezcruz@gmail.com>
2022-10-19 17:50:45 -0500

    task 9

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains the Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """The Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Square constructor"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """Return a Square definition"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.size)
