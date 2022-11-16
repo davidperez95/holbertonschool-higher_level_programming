040d1c108a0a674a38d81518aa65b8041aff0b97
David Perez <daperezcruz@gmail.com>
2022-11-16 12:45:53 -0500

    task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_usa
+-- creates the table states (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256)
+);
