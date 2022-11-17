dd677e534bc264e215af4f819dc7865a6f0c4201
David Perez <daperezcruz@gmail.com>
2022-11-16 19:21:13 -0500

    task 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title, tv_shows.genre_id
+FROM tv_shows
+INNER JOIN tv_shows_genres
+ON tv_shows.id = tv_shows_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
