08c5d3491770cf69875ff8508803eb9aa1662616
David Perez <daperezcruz@gmail.com>
2022-12-21 16:29:11 -0500

    task 7

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-    let times = 0;
-    for (let i in list) {
-        if (list[i] === searchElement) {
-            times += 1;
-        }
+  let times = 0;
+  for (const i in list) {
+    if (list[i] === searchElement) {
+      times += 1;
     }
-    return times;
-}
+  }
+  return times;
+};
