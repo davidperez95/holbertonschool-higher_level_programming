1f9cf5050b9cb59c50770706bd1b273beb31f1af
David Perez <daperezcruz@gmail.com>
2022-12-20 23:38:29 -0500

    task 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const array = [];
+    for (let i = 0; i < this.width; i++) {
+      array.push('X');
+    }
+    const setense = array.join('');
+    for (let i = 0; i < this.height; i++) {
+      console.log(setense);
+    }
+  }
+
+  rotate() {
+    aux = this.height;
+    this.height = this.width;
+    this.width = aux;
+  }
+
+  bouble () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};
